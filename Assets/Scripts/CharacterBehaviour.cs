b41280b1a4621e8ba574061fc5ceb0f8499a3bde
Oussama BOUDHRI <oboudhri@oussamas-macbook-pro.home>
2016-09-13 23:11:16 +0200

    feat(game-2D): prepare basic 2D game with assets and colliders

---
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+	public float force = 10f;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")) {
+			anim.SetBool ("isJumping", true);
+			GetComponent<Rigidbody2D> ().AddForce (Vector2.up * force);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (coll.gameObject.tag == "Hill")
+			anim.SetBool ("isJumping", false);
+
+	}
+}
